37e83ada6d2e37285a1433e465e66187aea50097
Alfonsina <alfonsina.l@gmail.com>
2023-12-03 11:20:30 -0300

    feat: add icon to switch component

--- components/shared/CustomSwitch.jsx
+++ components/shared/CustomSwitch.jsx
@@ -1,13 +1,53 @@
+"use client";
 import { Switch } from "@nextui-org/react";
 
+const BellIcon = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    className={`h-6 w-6 ${className}`}
+    viewBox="0 0 24 24"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+    ></path>
+  </svg>
+);
+
+const BellOffIcon = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={`h-6 w-6 ${className}`}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M9.143 17.082a24.248 24.248 0 003.844.148m-3.844-.148a23.856 23.856 0 01-5.455-1.31 8.964 8.964 0 002.3-5.542m3.155 6.852a3 3 0 005.667 1.97m1.965-2.277L21 21m-4.225-4.225a23.81 23.81 0 003.536-1.003A8.967 8.967 0 0118 9.75V9A6 6 0 006.53 6.53m10.245 10.245L6.53 6.53M3 3l3.53 3.53"
+    />
+  </svg>
+);
+
 export const CustomSwitch = () => {
-    return (
-        <Switch
-            defaultSelected
-            size="md"
-            color="secondary"
-            thumbIcon={''} // TO DO: add icon
-        >
-        </Switch>
-    )
-}
+  return (
+    <Switch
+      defaultSelected
+      size="md"
+      color="secondary"
+      thumbIcon={({ isSelected, className }) =>
+        isSelected ? (
+          <BellIcon className={className} />
+        ) : (
+          <BellOffIcon className={className} />
+        )
+      } // TO DO: add icon
+    ></Switch>
+  );
+};
