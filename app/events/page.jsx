e78a164c8878ddf13741b5a245644e9fd42b9fd4
Alfonsina <alfonsina.l@gmail.com>
2023-12-03 10:54:27 -0300

    feat: add events and select to event page

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import SelectEvent from "../../components/events/SelectEvent";
+import { CardEvent } from "../../components/events/CardEvent";
 
 const Events = () => {
   return (
     <section>
-      <h1>Events</h1>
+      <h1>Follow a new event</h1>
+      <SelectEvent />
+      <section className="flex flex-col items-center gap-x-5 px-2 md:flex-row md:flex-wrap">
+        <CardEvent />
+        <CardEvent />
+      </section>
     </section>
   );
 };
