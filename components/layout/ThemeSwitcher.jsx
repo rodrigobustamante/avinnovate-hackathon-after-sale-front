1fe16721aedbfad66e9d9b53d69dd4e5163fc925
Alfonsina <alfonsina.l@gmail.com>
2023-12-03 12:50:00 -0300

    refactor: remove commented code

--- components/layout/ThemeSwitcher.jsx
+++ components/layout/ThemeSwitcher.jsx
@@ -26,10 +26,5 @@ export function ThemeSwitcher() {
       onValueChange={setIsSelected}
       color="success"
     />
-    // <div>
-    //   The current theme is: {theme}
-    //   <button onClick={() => setTheme("light")}>Light Mode</button>
-    //   <button onClick={() => setTheme("dark")}>Dark Mode</button>
-    // </div>
   );
 }
